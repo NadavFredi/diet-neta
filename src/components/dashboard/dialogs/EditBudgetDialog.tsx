4783dc1b1743a9646109e0188fdbd360f91a8fec
Andrey <office@easyflow.co.il>
2026-01-17 15:54:21 +0200

    budget screen size fix

--- src/components/dashboard/dialogs/EditBudgetDialog.tsx
+++ src/components/dashboard/dialogs/EditBudgetDialog.tsx
@@ -26,13 +26,13 @@ export const EditBudgetDialog = ({
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange} modal={true}>
       <DialogContent 
-        className="max-w-6xl w-[95vw] max-h-[90vh] flex flex-col p-0 overflow-hidden" 
+        className="!max-w-[98vw] !w-[98vw] !h-[95vh] !max-h-[95vh] flex flex-col p-0 overflow-hidden !translate-x-[-50%] !translate-y-[-50%] !left-[50%] !top-[50%] !rounded-none" 
         dir="rtl"
       >
-        <DialogHeader className="px-4 pt-4 pb-3 border-b flex-shrink-0">
+        <DialogHeader className="px-6 pt-6 pb-4 border-b flex-shrink-0">
           <DialogTitle className="text-base font-bold">עריכת תקציב: {editingBudget.name}</DialogTitle>
         </DialogHeader>
-        <div className="overflow-y-auto min-h-0 px-4 py-3">
+        <div className="flex-1 overflow-y-auto min-h-0 px-6 pb-6">
           <BudgetForm
             mode="edit"
             initialData={editingBudget}
